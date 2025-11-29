c5e0fdcc6c5c4b7d28dd3a658caf7d95090d9016
Joshua Brady <josh@Joshuas-Air-3.lan>
2025-11-28 19:49:54 -0500

    Added UnionFind

--- tools.js
+++ tools.js
@@ -8,4 +8,34 @@ function convertToMatrix(person, hours){
 
 }
 
-export {getOffset, convertToMatrix, constructGrid};
+class UnionFind {
+    constructor(n) {
+        this.parent = new Array(n).fill(0).map((_, i) => i);
+        this.rank = new Array(n).fill(0);
+    }
+
+    find(x) {
+        if (this.parent[x] !== x) {
+            this.parent[x] = this.find(this.parent[x]);
+        }
+        return this.parent[x];
+    }
+
+    union(a, b) {
+        let rootA = this.find(a);
+        let rootB = this.find(b);
+
+        if (rootA !== rootB) {
+            if (this.rank[rootA] < this.rank[rootB]) {
+                this.parent[rootA] = rootB;
+            } else if (this.rank[rootA] > this.rank[rootB]) {
+                this.parent[rootB] = rootA;
+            } else {
+                this.parent[rootB] = rootA;
+                this.rank[rootA]++;
+            }
+        }
+    }
+}
+
+export {getOffset, convertToMatrix, UnionFind};
