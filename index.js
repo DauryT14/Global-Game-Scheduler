15ed3dddfde6a3a944e70ad94a66b564710cf644
DauryT14 <daurytoribio0@gmail.com>
2025-11-30 14:19:14 -0500

    added some functionality to the website. Made the create button actually redirect the user to the graph.html where they could see the best times and stuff like that.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 document.getElementById("eventForm").addEventListener('submit', function(event){
     event.preventDefault();
+    createEvent();
 });
+
+function createEvent(){
+    const eventName = document.getElementById("event").value.trim();
+    const startTime = document.getElementById("startTime").value;
+    const endTime = document.getElementById("endTime").value;
+    const dateInput = document.getElementById("date").value;
+    
+    if (eventName == ""){
+        alert('Please enter an event name!');
+        return;
+    }
+
+    if (startTime == endTime){
+        alert('Please enter a valid start time and end time');
+        return;
+    }
+
+    if (dateInput == ""){
+        alert('Please enter a date!');
+        return;
+    }
+
+    window.location.href = "graph.html";
+}
