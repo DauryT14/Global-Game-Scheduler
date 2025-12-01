9ed61424cf9e1ca07a04a896abdae8df48930497
Joshua Brady <josh@Joshuas-Air-3.lan>
2025-11-30 21:11:55 -0500

    Updates to test

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-//import data from './data.json' assert {type: 'json'};
-//import {getOffset, getTZIdentifier} from './tools.js'
+import data from './data.json' assert {type: 'json'};
+import {getOffset, getTZIdentifier} from './tools.js'
 
-//var person = data
+var person = data
 
-//console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
-//console.log(Date.UTC())
+console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
+console.log(Date.UTC())
 
-//console.log(getOffset())
+console.log(getOffset())
 
 import { UnionFind } from './tools.js';
 
@@ -16,7 +16,7 @@ uf.union(1, 2);
 uf.union(2, 3);
 uf.union(5, 6);
 
-console.log("1 and 3 connected? ", uf.find(1) === uf.find(3));  // true
-console.log("1 and 5 connected? ", uf.find(1) === uf.find(5));  // false
+console.log("1 and 3 connected? ", uf.find(1) === uf.find(3));
+console.log("1 and 5 connected? ", uf.find(1) === uf.find(5));
 
 console.log("Parents:", uf.parent);
