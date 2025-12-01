a5af88d73aaaefa8426f17cb6dc6672a2dc162fb
Joshua Brady <josh@Joshuas-Air-3.lan>
2025-11-30 20:54:18 -0500

    Save local changes before pulling

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
-import data from './data.json' assert {type: 'json'};
-import {getOffset, getTZIdentifier} from './tools.js'
+//import data from './data.json' assert {type: 'json'};
+//import {getOffset, getTZIdentifier} from './tools.js'
 
-var person = data
+//var person = data
 
-console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
-console.log(Date.UTC())
+//console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
+//console.log(Date.UTC())
 
-console.log(getOffset())
+//console.log(getOffset())
 
+import { UnionFind } from './tools.js';
+
+let uf = new UnionFind(10);
+
+uf.union(1, 2);
+uf.union(2, 3);
+uf.union(5, 6);
+
+console.log("1 and 3 connected? ", uf.find(1) === uf.find(3));  // true
+console.log("1 and 5 connected? ", uf.find(1) === uf.find(5));  // false
+
+console.log("Parents:", uf.parent);
